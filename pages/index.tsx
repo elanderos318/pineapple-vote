ab0709684cf1991e1cbd6f2ed5f4ca18ec8ebfe3
Eduardo Landeros <elanderos318@gmail.com>
2023-04-20 14:30:10 -0500

    update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+export default function Home() {   
     return (
-        <div>Hello!</div>
+        <div
+            style={{
+                display: 'flex',
+                height: "100vh",
+                width: "100vw",
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+            >
+                <button style={{ fontSize: 32, marginRight: 8 }}>yes</button>
+                <button style={{ fontSize: 32, marginLeft: 8 }}>no</button>
+            </div>
     )
 }
+
