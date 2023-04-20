4598a10402941959d3121ed7b8b0c4d0a2640689
Eduardo Landeros <elanderos318@gmail.com>
2023-04-20 14:47:24 -0500

    move clientApp logic to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,31 @@
-export default function Home() {   
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { useCollection } from "react-firebase-hooks/firestore"
+
+export default function Home() {  
+    const clientCredentials = {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    };
+    
+    if (!firebase.apps.length) {
+        firebase.initializeApp(clientCredentials);
+    }
+
+    const [votes, votesLoading, votesError ] = useCollection(
+        firebase.firestore().collection("votes"),
+        {}
+    )
+
+    if (!votesLoading && votes) {
+        votes?.docs.map((doc) => console.log(doc.data()))
+    }
+
     return (
         <div
             style={{
