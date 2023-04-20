fc6a6e0591df274cc372474e4493a2dcb8e1f4d2
Eduardo Landeros <elanderos318@gmail.com>
2023-04-20 15:51:22 -0500

    add user to collection on signup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const createUserDocument = functions.auth.user().onCreate((user) => {
+    db.collection("users").doc(user.uid).set(JSON.parse(JSON.stringify(user)));
+});
